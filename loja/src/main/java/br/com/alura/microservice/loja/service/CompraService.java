9e01421274b533843e6c80d54bad04f6e396262a
Herik dos Santos Lorenção <herik.lorencao@itixti.com.br>
2021-11-22 16:35:33 -0300

    analisando comportamento do load balanced

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -3,6 +3,7 @@ package br.com.alura.microservice.loja.service;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,8 +14,15 @@ public class CompraService {
 	@Autowired
 	private RestTemplate client;
 
+	@Autowired
+	private DiscoveryClient eurekaClient;
+
 	public void realizaCompra( CompraDTO compra ) {
 		var exchange = client.exchange( "http://fornecedor/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedorDTO.class );
+
+		eurekaClient.getInstances( "fornecedor" ).stream()
+				.forEach( endereco -> System.out.println( endereco.getHost() + ":" + endereco.getPort() ) );
+
 		System.out.println( exchange.getBody().getEndereco() );
 	}
 }
